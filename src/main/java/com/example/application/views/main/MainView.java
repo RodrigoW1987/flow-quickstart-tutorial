50d1a50ee407e7cc0ea8c4007681fb50425c18ed
RodrigoW1987 <105087785+RodrigoW1987@users.noreply.github.com>
2023-09-18 23:47:30 +0000

    .

--- src/main/java/com/example/application/views/main/MainView.java
+++ src/main/java/com/example/application/views/main/MainView.java
@@ -1,9 +1,17 @@
 package com.example.application.views.main;
 
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.contextmenu.MenuItem;
+import com.vaadin.flow.component.contextmenu.SubMenu;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.menubar.MenuBar;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -22,13 +30,42 @@ public class MainView extends VerticalLayout {
         });
         addButton.addClickShortcut(Key.ENTER);
 
+        MenuBar menuBar = new MenuBar();
+        Text selected = new Text("");
+        ComponentEventListener<ClickEvent<MenuItem>> listener = e -> selected
+                .setText(e.getSource().getText());
+        Div message = new Div(new Text("Clicked item: "), selected);
+
+        menuBar.addItem("View", listener);
+        menuBar.addItem("Edit", listener);
+
+        MenuItem share = menuBar.addItem("Share");
+        SubMenu shareSubMenu = share.getSubMenu();
+        MenuItem onSocialMedia = shareSubMenu.addItem("On social media");
+        SubMenu socialMediaSubMenu = onSocialMedia.getSubMenu();
+        socialMediaSubMenu.addItem("Facebook", listener);
+        socialMediaSubMenu.addItem("Twitter", listener);
+        socialMediaSubMenu.addItem("Instagram", listener);
+        shareSubMenu.addItem("By email", listener);
+        shareSubMenu.addItem("Get Link", listener);
+
+        MenuItem move = menuBar.addItem("Move");
+        SubMenu moveSubMenu = move.getSubMenu();
+        moveSubMenu.addItem("To folder", listener);
+        moveSubMenu.addItem("To trash", listener);
+
+        menuBar.addItem("Duplicate", listener);
+
         add(
                 new H1("Vaadin Todo"),
                 todosList,
                 new HorizontalLayout(
                         taskField,
                         addButton
-                )
+                ),
+                new Span("Um texto qualquer"),
+                menuBar 
         );
+        
     }
 }
